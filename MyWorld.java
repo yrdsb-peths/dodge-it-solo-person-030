997a8e3334a0b66e3c93e6e749269755a4edc6a5
person-030 <abigailyang783@gmail.com>
2025-11-04 14:20:58 -0500

    Apple and Bob

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,12 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Hero bob = new Hero();
+        addObject(bob, 100, 100);
+        
+        Apple apple = new Apple();
+        addObject(apple, 400, 100);
+        
     }
 }
